5f8324ffa1fb005f2606e46538a44db97b5d8a67
nargiza <sultanmuratova500@gmail.com>
2022-03-01 13:11:23 +0600

    clcode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,52 +3,59 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Planet planet=Planet.EARTH;
-        Planet planet1=Planet.MARS;
-        Planet planet2=Planet.JUPITER;
-        Planet planet3=Planet.MERCURY;
-        Planet planet4=Planet.NEPTUNE;
-        Planet planet5=Planet.SATURN;
-        Planet planet6=Planet.URANUS;
-        Planet planet7=Planet.VENUS;
+        Planet planet = Planet.EARTH;
+        Planet planet1 = Planet.MARS;
+        Planet planet2 = Planet.JUPITER;
+        Planet planet3 = Planet.MERCURY;
+        Planet planet4 = Planet.NEPTUNE;
+        Planet planet5 = Planet.SATURN;
+        Planet planet6 = Planet.URANUS;
+        Planet planet7 = Planet.VENUS;
 
-        Planet []planets={planet, planet1, planet2, planet3, planet4, planet5, planet6, planet7};
+        Planet[] planets = {planet, planet1, planet2, planet3, planet4, planet5, planet6, planet7};
 
-        Planet min=findAPlanetThatIsLessThanDays(planets);
-        Planet max=findAPlanetThatIsMoreThanDays(planets);
-        System.out.println("MIN: "+min+"- "+min.getDayOfAYear());
-        System.out.println("MAX: "+max+"- "+ max.getDayOfAYear());
-
-
+        Planet min = findAPlanetThatIsLessThanDays(planets);
+        Planet max = findAPlanetThatIsMoreThanDays(planets);
+        System.out.println("MIN: " + min + "- " + min.getDayOfAYear());
+        System.out.println("MAX: " + max + "- " + max.getDayOfAYear());
 
 
-    }public static Planet findAPlanetThatIsLessThanDays(Planet[]planets){
-        Planet minimum=null;
-        for (Planet pl: planets) {
-            if(minimum==null){
-                minimum=pl;
+    }
+
+    public static Planet findAPlanetThatIsLessThanDays(Planet[] planets) {
+        Planet minimum = null;
+        for (Planet pl : planets) {
+            if (minimum == null) {
+                minimum = pl;
                 continue;
-            }if(minimum!=null){
-                if (minimum.getDayOfAYear()> pl.getDayOfAYear()){
-                    minimum=pl;
+            }
+            if (minimum != null) {
+                if (minimum.getDayOfAYear() > pl.getDayOfAYear()) {
+                    minimum = pl;
                 }
             }
 
-        }return minimum;
+        }
+        return minimum;
 
 
-    }public static Planet findAPlanetThatIsMoreThanDays(Planet[]planets){
-        Planet maximum=null;
-        for (Planet pl: planets) {
-            if(maximum==null){
-                maximum=pl;
+    }
+
+    public static Planet findAPlanetThatIsMoreThanDays(Planet[] planets) {
+        Planet maximum = null;
+        for (Planet pl : planets) {
+            if (maximum == null) {
+                maximum = pl;
                 continue;
-            }if(maximum!=null){
-                if (maximum.getDayOfAYear()< pl.getDayOfAYear()){
-                    maximum=pl;
+            }
+            if (maximum != null) {
+                if (maximum.getDayOfAYear() < pl.getDayOfAYear()) {
+                    maximum = pl;
                 }
             }
 
-        }return maximum;
+        }
+        return maximum;
 
-}}
+    }
+}
